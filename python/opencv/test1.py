357dcfe1980683fe887c792cdce78b078ac9cde6
archer_wu <archer_wu@realsil.com.cn>
2019-01-31 09:28:10 +0800

    py

--- python/opencv/test1.py
+++ python/opencv/test1.py
@@ -3,10 +3,23 @@ import numpy as np
 import cv2 as cv
 from cvClass import *
 
-# img=cv.imread('cut.png')
+#img=cv.imread('cut.png')
+img=cv.imread('color.jpg')
+
+#np.set_printoptions(suppress=True)
+originCCM=np.mat([[332,-76,0],[-64,400,-80],[0,-172,428]])
+temp=CCM(1,60,originCCM)
+#temp.prtSome()
+temp.adjPic(img)
+cv.imshow('cut.png',img)
+
+#img=cv.imread('33.png')
 # prtImgPrpt(img)
 # for pixel in img[0:4, 0:1]:
 	# print(pixel)
+#cv.namedWindow('window7')
+#click=DrawClass('window7', img)
+#prtImgPrpt(img)
 
 # ball = img[380:440, 330:390]
 # img[273:333, 100:160] = ball
@@ -31,19 +44,21 @@ from cvClass import *
 # video1=VideoClass(videoAddr)
 # video1.playVideo('RealCam')
 
-img1 = cv.imread('cut.png')
-img2 = cv.imread('cut2.png')
-cv.namedWindow('dstWd')
-bar1=TrackBarClass(img1,img2,'dstWd')
+# img1 = cv.imread('cut.png')
+# img2 = cv.imread('cut2.png')
+# cv.namedWindow('dstWd')
+# bar1=TrackBarClass(img1,img2,'dstWd')
 
 while(1):
-	#dst = cv.addWeighted(img1,t,img2,k,0)
-	cv.imshow('dstWd',bar1.imgBlend())
-	#print("changed")
-	
-	k = cv.waitKey(1) & 0xFF
+	# #dst = cv.addWeighted(img1,t,img2,k,0)
+	# #cv.imshow('dstWd',bar1.imgBlend())
+	# #print("changed")
+	# cv.imshow('window7', img)
+	k = cv.waitKey(1)
 	if k == 27:
 		break
+
+
 
 #cv.waitKey(0)
 cv.destroyAllWindows()
